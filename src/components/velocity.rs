eb0875cb97c194a1535b968434cda9d295ddcbf6
Masa Maeda <masamaedae@gmail.com>
2020-05-24 13:54:12 -0700

    Fixes comment issues

--- src/components/velocity.rs
+++ src/components/velocity.rs
@@ -2,7 +2,7 @@ use amethyst::core::math::Vector3;
 use amethyst::ecs::prelude::*;
 
 pub struct Velocity {
-    pub velocity: Vector3<f32>,
+    pub value: Vector3<f32>,
 }
 
 impl Component for Velocity {
